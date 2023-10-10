01b79e6bfcc5fa9d6cf1cdd326c4028f976d6398
izabayo7 <54701788+izabayo7@users.noreply.github.com>
2023-10-10 17:19:23 +0200

    chore: changed base.html to index

--- standalone-frontend/app.js
+++ standalone-frontend/app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('http://146.190.40.172:5001/predict', {
+        fetch('https://api.ikaye.rw/chatbot/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
